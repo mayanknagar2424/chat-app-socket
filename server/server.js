55b975fb60d4adb4209659e1a30df053fa00d749
Mayank Nagar 2 <maynagar@publicisgroupe.net>
2018-09-24 00:18:13 +0530

    sockets commites

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express();
 
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+//middleware
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection',(socket)=>{
+console.log('New user connection');
+
+socket.on('disconnect',()=>{
+    console.log('User was disconnected');
+});
+
+});
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
